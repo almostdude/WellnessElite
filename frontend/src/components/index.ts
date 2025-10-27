151dbde770b1f7f1c95a9914f0dc513e3c398bce
almostdude <almostdude@proton.me>
2025-10-27 19:43:32 +0100

    Add index.ts

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,6 @@
+export { ImageDisplay } from './ImageDisplay';
+export { IPFSUpload } from './IPFSUpload';
+export { ZamaIntegration } from './ZamaIntegration';
+export { DecryptImage } from './DecryptImage';
+export { UserImageList } from './UserImageList';
+export { ImageCard } from './ImageCard';
